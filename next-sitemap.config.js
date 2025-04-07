429315d597796e82c66bd5c7e77ecbb501b40759
vibeBill <bill.zhu@vibe.us>
2025-04-07 17:59:36 +0800

    feat: update

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://convert.bill-aurora.ip-ddns.com',
+  generateRobotsTxt: true, // 可选：同时生成robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  outDir: 'public',
+}
