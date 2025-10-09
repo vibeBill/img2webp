de09d4fe65a1f2e50d2fdf95e2709fb72a1b0f39
vibeBill <bill.zhu@vibe.us>
2025-10-09 16:44:29 +0800

    fix: 修复静态文件访问不到的问题

--- src/middleware.ts
+++ src/middleware.ts
@@ -52,5 +52,7 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+  ],
 };
